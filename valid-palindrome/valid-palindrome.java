83490765f60a812b9f6f78b00bb4f78583181005
Mireia Gómez Diaz <76046991+migodi15@users.noreply.github.com>
2021-11-30 12:22:57 +0100

    Time: 25 ms (25.12%), Space: 40.3 MB (36.64%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) return false;
+        }
+        
+        return true;
+    }
+}
