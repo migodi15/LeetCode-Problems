99e299861576f91d9042e8ff65492f83f706766c
Mireia Gómez Diaz <76046991+migodi15@users.noreply.github.com>
2021-12-02 09:52:08 +0100

    Time: 11 ms (69.94%), Space: 38.7 MB (83.57%) - LeetHub

--- power-of-three/power-of-three.java
+++ power-of-three/power-of-three.java
@@ -1,10 +1,22 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        double x = Math.log10(n) / Math.log10(3);
+        if (n <= 0) return false;
         
-        //is integer
-        if (x % 1 == 0) return true;
+        double power = Math.log10(n) / Math.log10(3);                          
+        if (power == Math.ceil(power)) return true;          
         
         return false;
+        
+        /* Recursivity
+        
+        if (n == 0) {
+            return false;
+        } else if (n == 1) {
+            return true;
+        } else if (n % 3 == 0) {
+            return isPowerOfThree(n/3);
+        }
+        return false;
+        */
     }
 }
