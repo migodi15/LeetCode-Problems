0cc416c7cd994d1a04f9573d87271ceabec16ea7
Mireia Gómez Diaz <76046991+migodi15@users.noreply.github.com>
2023-11-13 13:17:03 +0100

    Time: 0 ms (100.00%), Space: 38.72 MB (88.88%) - LeetHub

---
+++ climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 1;
+
+        // fibonacci
+        for (int i = 0; i < n; i++) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+
+        return n3;
+    }
+}
